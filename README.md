f9d386a385aa109b4db6bcc8c170aa8efbacc78b
shenshuo <accounts_6549efe4970dc802e55f86fe@mail.teambition.com>
2023-12-12 11:14:01 +0800

    feat: 添加swipeAction的demo

---
+++ README.md
@@ -0,0 +1,22 @@
+本 demo 旨在复现 taro-ui 的组件 SwipeAction 在手机端会导致页面无法滚动的问题，电脑浏览器没有问题。手机浏览器，微信浏览器都有问题。
+
+出现问题的手机型号有：mate20pro（鸿蒙2.0.0）、一加 Ace 竞速版（ColorOS 13.1）
+
+复现视频在项目的 screenShots 目录下（mate20pro）
+
+### 项目依赖的部分说明
+```
+@tarojs/cli: 3.6.19
+
+nodejs: v16.20.2
+```
+
+### 安装
+```
+yarn install
+```
+
+### 启动
+```
+yarn dev:h5
+```
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,51 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components';
+import { AtSwipeAction } from 'taro-ui';
 import './index.less'
 
-export default function Index() {
-
-  useLoad(() => {
-    console.log('Page loaded.')
-  })
+export default function AiChat() {
+  const list = [
+    '我是标题',
+    '我是标题',
+    '我是标题',
+    '我是标题',
+    '我是标题',
+    '我是标题',
+    '我是标题',
+    '我是标题',
+    '我是标题',
+    '我是标题',
+    '我是标题',
+    '我是标题',
+  ]
 
   return (
-    <View className='index'>
-      <Text>Hello world!</Text>
+    <View className='scroll'>
+      {
+        list.map((item: any, index: number) => (
+          <AtSwipeAction
+            key={index}
+            autoClose
+            options={[
+              {
+                text: '删除',
+                style: {
+                  backgroundColor: '#4C4C4C'
+                },
+              }
+            ]}
+          >
+            <View
+              className='item'
+              key={index}
+            >
+              <Text className='word'>{item.slice(0, 1)}</Text>
+              <View className='wrapper'>
+                <View className='title'>{item}</View>
+              </View>
+            </View>
+          </AtSwipeAction>
+        ))
+      }
     </View>
   )
 }
